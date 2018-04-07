7ea2b8d6256f19c6c2b7a3b351cc0ce4da324ed4
hailing-li <hailing@hawaii.edu>
2018-04-06 20:38:20 -1000

    Add files via upload

---
+++ green_send.php
@@ -0,0 +1,70 @@
+
+<html>
+<head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css">
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.js"></script>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php
+    $host = 'localhost';
+    $user = 'gviloria_pango';
+    $pass = 'teampangolin';
+    $db = 'gviloria_ALERT';
+            $dbConnection = new mysqli($host, $user, $pass, $db);
+            if (mysqli_connect_errno()) {
+                printf("Could not connect to the mySQL database: %s\n", mysqli_connect_error());
+                exit();
+            }
+            // Basic Select Statement to check if the table exists
+            $query = "SELECT * FROM SYSTEMDATA";
+            $result = mysqli_query($dbConnection, $query);
+            $num = $result->num_rows;
+            $i = 0;
+            function mysqli_result($result, $row, $field = 0)
+            {
+                // Adjust the result pointer to that specific row
+                $result->data_seek($row);
+                // Fetch result array
+                $data = $result->fetch_array();
+                return $data[$field];
+            }
+            while ($i < $num) {
+                $warning = mysqli_result($result, $i, "Warning");
+                $disaster = mysqli_result($result, $i, "Disaster");
+                $alert = mysqli_result($result, $i, "Alert");
+                $i++;
+            
+$to ="8085896694@tmomail.net";
+$email= "hailing@hawaii.edu";
+$sharie="8083084501@tmomail.net";
+$gabe="8082189262@tmomail.net";
+$from = "demo@gviloria.ics415.com";
+$message ="$disaster Alert\nThis is a $disaster alert, please find a refuge as soon as possible! \nThis alert is not real, it's just a test!";
+}
+$headers = "From: $from\n";
+mail($to, 'subject', $message, $headers);
+mail($sharie, 'subject', $message, $headers);
+mail($gabe, 'subject', $message, $headers);
+mail($email, 'subject', $message, $headers);
+;?>
+<div class="ui container" id="main">
+<div class="ui center aligned segment">
+  <div class="ui green inverted raised segment">
+    <h2>TEST ALERT SENT SUCCESSFULLY！</h2>
+  </div>
+  <H3>Dsaster Type: <?php echo $disaster ?> </H3>
+  <H3>Alert Type: 
+      <?php for($x = 0; $x < count($alert); $x++) {
+         echo $alert[$x];
+         echo "; ";
+        } ?> 
+  </H3>
+  </br></br>
+  <a href="main.php" style="text-decoration: underline;">&lt;Back to Home Page</a>
+</div>
+    
+</div>
+</body>
+</html>
---
+++ greendisaster.php
@@ -0,0 +1,182 @@
+<html lang="en">
+
+<head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css">
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.js"></script>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1 style="text-align: center;">EMERGENCY ALERT SYSTEM</h1>
+
+<div class="ui container" id="main">
+  <div class="ui green inverted raised segment">
+    <h2 id="header">DISASTER TYPES</h2>
+  </div>
+
+<form method="post" action="adddisasterdb.php">
+    
+<?php
+    $host = 'localhost';
+    $user = 'gviloria_pango';
+    $pass = 'teampangolin';
+    $db = 'gviloria_ALERT';
+	
+    $dbConnection = new mysqli($host, $user, $pass, $db);
+	
+    if (mysqli_connect_errno()) {
+        printf("Could not connect to the mySQL database: %s\n", mysqli_connect_error());
+        exit();
+    }
+
+    $query = "SELECT * FROM SYSTEMDATA WHERE id = 1";
+    $result = mysqli_query($dbConnection, $query);
+    $num = $result->num_rows;
+    $i = 0;
+    function mysqli_result($result, $row, $field = 0)
+    {
+        // Adjust the result pointer to that specific row
+        $result->data_seek($row);
+        // Fetch result array
+        $data = $result->fetch_array();
+        return $data[$field];
+    }
+    while ($i < $num) {
+        $id = mysqli_result($result, $i, "id");
+        $warning = mysqli_result($result, $i, "Warning");
+        $disaster = mysqli_result($result, $i, "Disaster");
+        $alert = mysqli_result($result, $i, "Alert");
+        $i++;
+    }
+    mysqli_close($dbConnection);
+?>
+    
+<div class="ui raised segment">
+<div class="content">
+<div class="ui grid">
+  <div class="centered row" style="margin:2em;">
+    <div class="five wide column">
+      <div class="border">
+      <input class="ui button disaster_type" type="radio" id="surf" name="Disaster" value="surf">
+        <span class="disaster">High Surf</p>
+      </div>
+    </div>
+    <div class="five wide column">
+      <div class="border">
+      <input class="ui button disaster_type" type="radio" id="land" name="Disaster" value="landslide">
+        <span class="disaster">Landslide</p>
+      </div>
+    </div>
+    <div class="five wide column">
+      <div class="border">
+        <input class="ui button disaster_type"  type="radio" id="tsunami" name="Disaster" value="tsunami">
+        <span class="disaster">Tsunami</p>
+      </div>
+    </div>
+  </div> <!end of 1st row>
+  <div class="centered row" style="margin:0 0 2em 0;">
+    <div class="five wide column">
+      <div class="border">
+      <input class="ui button disaster_type" type="radio" id="amber" name="Disaster" value="alert">
+        <span class="disaster">Amber Alert</p>
+      </div>
+    </div>
+    <div class="five wide column">
+      <div class="border">
+      <input class="ui button disaster_type" type="radio" id="missile" name="Disaster" value="missile">
+        <span class="disaster">Ballistic Missile</span>
+      </div>
+    </div>
+    <div class="five wide column">
+      <div class="border">
+      <input class="ui button disaster_type" type="radio" id="volcano" name="Disaster" value="volcano">
+        <span class="disaster" name="Disaster" value="volcano">Volcano</p>
+      </div>
+    </div>
+  </div> <!end of 2nd row>
+  <div class="four column row">
+    <div class="ui right floated column">
+      <input class="ui button disaster" type="submit" value="Submit">
+    </div>
+  </div> <!end of column and 2nd row>
+</div><!end of  grid>
+</div>
+</div>
+
+</form>
+
+</div>
+
+<script>
+
+  var disaster = " ";
+  var i = 0;
+
+//   $('.ui .item').on('click', function() {
+//     $('.ui .item').removeClass('active');
+//     $(this).addClass('active');
+//   });
+  
+  function disasterType(typeDis) {
+      var isBorder;
+      
+      disaster = typeDis;
+      console.log(typeDis);
+      
+      switch(disaster) {
+          case "surf":
+              makeSelection(0);
+              break;
+          case "land":
+              makeSelection(1);
+              break;
+          case "tsunami":
+              makeSelection(2);
+              break;
+          case "amber":
+              makeSelection(3);
+              break;
+          case "missile":
+              makeSelection(4);
+              break;
+          case "volcano":
+              makeSelection(5);
+              break;
+          default:
+              break;
+      }
+      
+      isMethodSelected();
+  }
+  
+  function makeSelection(num) {
+      isBorder = document.getElementsByClassName("border");
+      isBorder[num].style.border = "1px solid black";
+        
+      for(var i = 0; i < isBorder.length; i++) {
+        if(isBorder[i].style.border == "1px solid black" && i != num) {
+            isBorder[i].style.border = "none";
+        }
+      }
+  }
+  
+  function isMethodSelected() {
+      var color;
+      
+      if(disaster != "" && i == 0) {
+        i++;
+        color = document.getElementsByClassName("ui button disaster");  
+        color[0].className = "ui blue button disaster";
+      }
+  }
+  
+  function redirect() {
+    if(disaster != "") {
+        location.href = "greenalert.php";
+    }
+  }
+  
+</script>
+
+</body>
+</html>
