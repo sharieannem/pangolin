037995aa01e6310d867fb1fba86198ba3217a658
hailing-li <hailing@hawaii.edu>
2018-04-26 21:01:18 -1000

    reorganized old implentation to a folder

---
+++ old implementation for a3/redaddwarningdb.php
@@ -0,0 +1,37 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+	
+    $host = 'localhost';
+    $user = 'gviloria_pango';
+    $pass = 'teampangolin';
+    $db = 'gviloria_ALERT';
+	
+    $dbConnection = new mysqli($host, $user, $pass, $db);
+	
+    if (mysqli_connect_errno()) {
+        printf("Could not connect to the mySQL database: %s\n", mysqli_connect_error());
+        exit();
+    }
+	
+    if($_POST) {
+		
+		$warning = $_POST["Warning"];
+        $disaster = $_POST["Disaster"];
+        $alert = $_POST["Alert"];
+      
+        if($results = $dbConnection->prepare("UPDATE SYSTEMDATA SET Warning = ? WHERE id = 1;")) {
+            $results->bind_param("s", $warning);
+            $results->execute();
+            $results->close();
+        } else {
+            echo 'Error';
+        }
+        
+        header("location: reddisaster.php");
+        mysqli_close($dbConnection);
+        exit();
+    }
+}
+
+?>
